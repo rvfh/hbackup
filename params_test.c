b32999468f040472c728082e0dd1042e2db91dff
herve.fache <herve.fache@11201afc-351b-0410-93e0-57f2dd02af1f>
2006-10-23 17:34:18 +0000

    Double quote support + helper functions

--- params_test.c
+++ params_test.c
@@ -34,5 +34,18 @@ int main(void) {
   printf("Read %u parameters from %s: '%s' '%s' '%s'\n",
     params_readline(line, keyword, type, string), line, keyword, type, string);
 
+  strcpy(line, "This is a text which I like");
+  printf("Converting '%s' to lower case\n", line);
+  strtolower(line);
+  printf("-> gives '%s'\n", line);
+
+  strcpy(line, "C:\\Program Files\\HBackup\\hbackup.EXE");
+  printf("Converting '%s' to linux style\n", line);
+  pathtolinux(line);
+  printf("-> gives '%s'\n", line);
+  printf("Then to lower case\n");
+  strtolower(line);
+  printf("-> gives '%s'\n", line);
+
   return 0;
 }
