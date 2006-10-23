97e5dcf398667c365a3fed8ff5af7c7bad68ace9
herve.fache <herve.fache@11201afc-351b-0410-93e0-57f2dd02af1f>
2006-10-23 14:39:11 +0000

    Parameters parsing

--- trunk/params.c
+++ trunk/params.c
@@ -3,9 +3,118 @@
 20061022 Creation
 */
 
+#define _GNU_SOURCE
+#include <stdio.h>
+#include <string.h>
 #include "params.h"
+
+static void remove_starting_blanks(char **string) {
+  while (((*string)[0] == ' ') || ((*string)[0] == '\t')) {
+    (*string)++;
+  }
+}
+
+static void remove_trailing_blanks(char *string) {
+  char *last = &string[strlen(string) - 1];
+
+  while ((last >= string) && ((*last == ' ') || (*last == '\t'))) {
+    *last-- = '\0';
+  }
+}
+
+static char *find_blank(const char *string) {
+  char *space = strchr(string, ' ');
+  char *tab = strchr(string, '\t');
+
+  if (space == NULL) {
+    return tab;
+  } else if (tab == NULL) {
+    return space;
+  } else if (space < tab) {
+    return space;
+  } else {
+    return tab;
+  }
+}
 
 int params_readline(const char *line, char *keyword, char *type,
     char *string) {
-  return 0;
+  char linecopy[FILENAME_MAX];
+  char *start = linecopy;
+  char *delim;
+  int param_count = 0;
+
+  /* Reset all */
+  strcpy(linecopy, line);
+  strcpy(keyword, "");
+  strcpy(type, "");
+  strcpy(string, "");
+
+  /* Get rid of comments */
+  if ((delim = strchr(start, '#')) != NULL) {
+    *delim = '\0';
+  }
+
+  /* Get rid of starting/trailing spaces and tabs */
+  remove_starting_blanks(&start);
+  remove_trailing_blanks(start);
+
+  /* Find next blank */
+  if ((delim = find_blank(start)) == NULL) {
+    delim = &start[strlen(start)];
+  }
+
+  /* Extract keyword */
+  strncpy(keyword, start, delim - start);
+  keyword[delim - start] = '\0';
+  if (keyword[0] != '\0') {
+    param_count++;
+  } else {
+    return param_count;
+  }
+
+  /* Go to next parameter */
+  start = delim;
+  remove_starting_blanks(&start);
+
+  /* Find next blank */
+  if ((delim = find_blank(start)) == NULL) {
+    delim = &start[strlen(start)];
+  }
+
+  /* Extract type or string */
+  strncpy(string, start, delim - start);
+  string[delim - start] = '\0';
+  if (string[0] != '\0') {
+    param_count++;
+  } else {
+    return param_count;
+  }
+
+  /* Go to next parameter */
+  start = delim;
+  remove_starting_blanks(&start);
+
+  /* Do we have more? */
+  if (start[0] == '\0') {
+    return param_count;
+  } else {
+    strcpy(type, string);
+  }
+
+  /* Find next blank */
+  if ((delim = find_blank(start)) == NULL) {
+    delim = &start[strlen(start)];
+  }
+
+  /* Extract string */
+  strncpy(string, start, delim - start);
+  string[delim - start] = '\0';
+  if (string[0] != '\0') {
+    param_count++;
+  } else {
+    return param_count;
+  }
+
+  return param_count;
 }
--- trunk/params_test.c
+++ trunk/params_test.c
@@ -6,5 +6,33 @@
 #include "params.c"
 
 int main(void) {
+  char line[FILENAME_MAX] = "";
+  char keyword[256];
+  char type[256];
+  char string[FILENAME_MAX];
+
+  printf("Read %u parameters from %s: '%s' '%s' '%s'\n",
+    params_readline(line, keyword, type, string), line, keyword, type, string);
+
+  strcpy(line, "# Normal comment");
+  printf("Read %u parameters from %s: '%s' '%s' '%s'\n",
+    params_readline(line, keyword, type, string), line, keyword, type, string);
+
+  strcpy(line, " \t# Displaced comment");
+  printf("Read %u parameters from %s: '%s' '%s' '%s'\n",
+    params_readline(line, keyword, type, string), line, keyword, type, string);
+
+  strcpy(line, "\tkey # Comment");
+  printf("Read %u parameters from %s: '%s' '%s' '%s'\n",
+    params_readline(line, keyword, type, string), line, keyword, type, string);
+
+  strcpy(line, "key\t \"string\" # Comment");
+  printf("Read %u parameters from %s: '%s' '%s' '%s'\n",
+    params_readline(line, keyword, type, string), line, keyword, type, string);
+
+  strcpy(line, "key \ttype\t \"string\" # Comment");
+  printf("Read %u parameters from %s: '%s' '%s' '%s'\n",
+    params_readline(line, keyword, type, string), line, keyword, type, string);
+
   return 0;
 }
