5a68183530b9842faafc5334ac98515caaca8b0e
herve.fache <herve.fache@11201afc-351b-0410-93e0-57f2dd02af1f>
2007-01-29 13:04:23 +0000

    Filter and Rule need destructors

--- filters.cpp
+++ filters.cpp
@@ -105,6 +105,18 @@ Rule::Rule(Condition *condition) {
   push_back(condition);
 }
 
+Rule::~Rule() {
+  for (unsigned int i = 0; i < size(); i++) {
+    delete (*this)[i];
+  }
+}
+
+Filter::~Filter() {
+  for (unsigned int i = 0; i < size(); i++) {
+    delete (*this)[i];
+  }
+}
+
 int Filter::match(const filedata_t *filedata) const {
   /* Read through list of rules */
   for (unsigned int i = 0; i < size(); i++) {
