8153c36ab8bcd18bfe41f6654ae6f66bf5956065
herve.fache <herve.fache@11201afc-351b-0410-93e0-57f2dd02af1f>
2007-04-16 19:37:28 +0000

    Something wrong went through

--- cli/hbackup.cpp
+++ cli/hbackup.cpp
@@ -39,12 +39,18 @@ using namespace std;
 
 /* DEFAULTS */
 
+/* Verbosity */
+static int verbose = 0;
+
 /* Configuration path */
 static string default_config_path = "/etc/hbackup/hbackup.conf";
 
 /* Configuration path */
 static string default_db_path = "/hbackup";
 
+
+/* Signal received? */
+static int killed = 0;
 
 static void show_version(void) {
   cout << "(c) 2006-2007 Hervé Fache, version "
@@ -67,6 +73,14 @@ static void show_help(void) {
 /etc/hbackup.conf" << endl;
   cout << " -s or --scan     to scan the database for missing data" << endl;
   cout << " -t or --check    to check the database for corrupted data" << endl;
+}
+
+int verbosity(void) {
+  return verbose;
+}
+
+int terminating(void) {
+  return killed;
 }
 
 void sighandler(int signal) {
