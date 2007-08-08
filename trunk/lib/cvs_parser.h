b7ddbd9a388c872a0fd74a1b148121705227163d
herve.fache <herve.fache@11201afc-351b-0410-93e0-57f2dd02af1f>
2007-08-08 15:03:25 +0000

    Work around GCC bug to get rid of vector in CVS too

--- trunk/lib/cvs_parser.h
+++ trunk/lib/cvs_parser.h
@@ -19,25 +19,13 @@
 #ifndef CVS_PARSER_H
 #define CVS_PARSER_H
 
-// List does not work
-#include <vector>
-
-using namespace std;
-
-#include "files.h"
 #include "parsers.h"
 
 namespace hbackup {
 
 class CvsParser : public Parser {
-  struct cvs_entry_t {
-    string  name;   /* File name */
-    char    type;   /* File type */
-  };
-
   static char*      _control_dir;
   static char*      _entries;
-  vector<cvs_entry_t> _files;
 public:
   // Constructor for parsers list
   CvsParser(parser_mode_t mode) : Parser(mode) {}
@@ -50,13 +38,7 @@ public:
   // That tells use whether to ignore the file, i.e. not back it up
   bool ignore(const File& file_data);
   // For debug purposes
-  void list() {
-    cout << "List: " << _files.size() << " file(s)" << endl;
-    vector<cvs_entry_t>::iterator i;
-    for (i = _files.begin(); i != _files.end(); i++) {
-      cout << "-> " << i->name << " (" << i->type << ")" << endl;
-    }
-  }
+  void list();
 };
 
 }
--- trunk/lib/test/cvs_parser_test.cpp
+++ trunk/lib/test/cvs_parser_test.cpp
@@ -19,7 +19,6 @@
 using namespace std;
 
 #include <iostream>
-#include <vector>
 #include <list>
 #include <sys/stat.h>
 
