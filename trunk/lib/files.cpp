5470b42f62ebedd6fe3f085e4df2c762af3c0345
herve.fache <herve.fache@11201afc-351b-0410-93e0-57f2dd02af1f>
2007-08-24 21:20:34 +0000

    files: repair pathCompare for non-printable characters, add test

--- trunk/lib/files.cpp
+++ trunk/lib/files.cpp
@@ -111,11 +111,19 @@ int Node::pathCompare(const char* s1, const char* s2, size_t length) {
             s1++;
             s2++;
           } else {
-            return -1;
+            if (*s2 < ' ') {
+              return 1;
+            } else {
+              return -1;
+            }
           }
         } else {
           if (*s2 == '/') {
-            return 1;
+            if (*s1 < ' ') {
+              return -1;
+            } else {
+              return 1;
+            }
           } else {
             if (*s1 < *s2) {
               return -1;
